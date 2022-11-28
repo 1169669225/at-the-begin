708e1c9409079d562a32869ac5a98740acc83126
1169669225 <119105646+1169669225@users.noreply.github.com>
2022-11-28 23:04:58 +0800

    Add files via upload

---
+++ c语言第三天.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+	int i, j= 0, k, n = 0, sumi = 0;
+	scanf_s("%d",&i);
+	for (k = 0; k >= ((1 + 2 * j) - 1) / 2; k++)//每层几个空格
+	{
+		printf(" ");
+	}
+	for (j = 1; j <= i; j++)//每一层的数字是几
+	{
+		
+		
+		for (n = (1+2*j); n <= (1 + 2 * j); n++)//每层几个数字
+		{
+			printf("j");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+练习5-7!!!!!!!
+#include <stdio.h>
+int main()
+{
+	int i, j, k, n, m;
+	printf("数据个数：");
+	scanf_s("%d",&i);
+	printf("1号：");
+	scanf_s("%d", &j);
+	printf("2号：");
+	scanf_s("%d", &k);
+	printf("3号：");
+	scanf_s("%d", &n);
+	printf("4号：");
+	scanf_s("%d", &m);
+	char a[]
+	return 0;
+}
+
+
+
