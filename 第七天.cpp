5ca1fd5936be17071e6860a129fe4e1600e3bee5
1169669225 <119105646+1169669225@users.noreply.github.com>
2022-12-03 22:26:05 +0800

    Add files via upload

---
+++ 第七天.cpp
@@ -0,0 +1,62 @@
+//#include <stdio.h>
+////递归实现n的k次方
+//int Pow(int n, int k)
+//{
+//	if (k == 0)
+//		return 1;
+//	if (k > 0)
+//		return n * Pow(n, k - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf_s("%d",&n);
+//	int ret = Pow(n, k);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	//int a = 10;
+//	//int arr[10] = {1,2,3,4,5,6,7,8,9,10};//完全初始化
+//	////int arr【10】 = {1，2，3，4，5}；//不完全初始化
+//	/*char ch3[5] = "bit";
+//	char ch4[] = {'b','i','t'};
+//	printf("%s\n",ch3);
+//	printf("%s\n", ch4);
+//	printf("%d\n", strlen(ch3));
+//	printf("%d\n", strlen(ch4));*/
+//	//int arr[10] = { 0,0,0,0,0,0,0,0 };
+//	//arr[4];//[]下标引用..数组名是数组首元素的地址
+//
+//    
+//
+//	return 0;	
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	//创建
+//	/*int arr[3][4];*/
+//	//初始化-创建的同时给赋值
+//	//int arr[3][4] = { 1,2,3,4,5,6,7 };//不完全初始化-后面补0
+//	/*int arr[][4] = { {1,2},{3,4},{4,5} };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}*/
+//	return 0;
+//}
