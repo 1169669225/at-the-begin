1f045ea3c1cc9785d083ba6cf4eff983a9a90b6a
1169669225 <119105646+1169669225@users.noreply.github.com>
2022-12-08 23:01:05 +0800

    Add files via upload

---
+++ 2022.12.8.the.twelfth.day.cpp
@@ -0,0 +1,59 @@
+// #include <iostream>
+// using namespace std;
+// //快速排序算法,边界问题用模板
+// const int N = 1e6 + 10;
+// int n;
+// int q[N];
+// void quick_sort(int q[],int l,int r)
+// {
+//     if(l >= r)
+//     return;
+//     int x = q[l],i = 1,j = r + 1;
+//     while(i < j)
+//     {
+//         do i++;
+//         while(q[i] < x);
+//         do j--;
+//         while(q[j] > x);
+//         if(i < j)
+//         {
+//             swap(q[i],q[j]);
+//         }
+//     }
+//     quick_sort(q,l,i - 1);
+//     quick_sort(q,j + 1,r);
+// }
+// int main()
+// {
+//      scanf("%d",&n);
+//      for(int i = 0;i < n;i++)
+//      {scanf("%d",&q[i]);}
+//      quick_sort(q,0,n - 1);
+//      for(int i = 0;i < n;i++)
+//      {printf("%d",&q[i]);}
+
+//     return 0;
+// }
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+//    long long a, b;
+//    scanf("%lld%lld", &a, &b);
+   //int :%d float:%f double:%lf char:%c long long:%lld
+    // int a = 6;
+    // int c = a++;
+    // printf("%d\n",c);
+    // int d = ++c;
+    // printf("%d\n",d);
+    // char c = 'a';
+    // int a = (int )c;
+    // printf("%d\n",a);
+    // char a = 'A';
+    // printf("%c\n",char (a + 2));
+    
+    return 0;
+}
