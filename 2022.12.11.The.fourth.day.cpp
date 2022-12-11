907f32fbdc12e3f81996748b9f461ebdf2c22b6e
1169669225 <119105646+1169669225@users.noreply.github.com>
2022-12-11 21:25:44 +0800

    Add files via upload

---
+++ 2022.12.11.The.fourth.day.cpp
@@ -0,0 +1,37 @@
+// #include <iostream>
+// #include <vector>//自带数组size函数
+// using namespace std;
+// const int N = 1e6 + 10;
+// vector<int> add(vector<int> &A,vector<Int> &B)
+// {
+//     vector<int> C;
+//     for(int i = 0;i < A.size() || i < B.size();i++)
+//     {
+//         if(i < A.size)
+//     }
+// }
+// int main()
+// {
+//     string a,b;
+//     vector<int> A,B;
+//     scanf("%d%d",&a,&b);
+//     for(int i = a;size()-1;i >= 0;i--) A.push_back([i]-'0');
+//     for(int i = B;size()-1;i >= 0;i--) B.push_back([i]-'0');
+//     auto C = add(A,B);
+//     for(int i = C,size() - 1;i >= 0;i--) printf("%d",c[i]);
+//     return 0;
+// }
+
+#include <iostream>//cin，cout
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <cstdio>//有printf,scanf
+using namespace std;
+
+int main()
+{
+    //输出多个变量  cout << a + b << ' ' << a * b << endl;
+    //float 6-7位 double 15-16位   根号sqrt
+    return 0;
+}
